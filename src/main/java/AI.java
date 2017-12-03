2a619d06d68cd0ee5941f6398d0b6a3d2d3fc52e
Payam Mohammadi <payam.int@gmail.com>
2017-12-03 15:39:03 +0330

    SNAPSHOT

--- src/main/java/AI.java
+++ src/main/java/AI.java
@@ -1,9 +1,7 @@
-package client;
-
 import client.ai.My;
 import ir.pint.soltoon.soltoongame.client.GameRunner;
 
-public class Payam {
+public class AI {
     public static void main(String[] args) {
         GameRunner.run(My.class);
     }
---
+++ src/main/java/ir/pint/soltoon/soltoongame/readme.md
@@ -0,0 +1,9 @@
+# SoltoonGame
+
+## Exit Codes
+
+| Error code  | Description  |
+| ----------- | ------------ | 
+|  0 | Successful exit            |
+| -1 | Socket connection problem  |
+| -2 | Potential Security Problem |
